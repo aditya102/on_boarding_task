fcab46e3efe60ff47f0f1952ad7631a8e205fb64
Aditya7861 <aditya9640@gmail.com>
2019-02-11 14:48:45 +0530

    Formate Test files

--- tests/test_user/test_login.py
+++ tests/test_user/test_login.py
@@ -25,24 +25,19 @@ class AccountsTestCase(TestCase):
         response = self.client.get(reverse('login'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'users/login.html')
-        self.assertContains(
-            response, '<div id="div_id_username" class="form-group"> <label for="id_username" class="col-form-label  requiredField">')
-        self.assertContains(
-            response, '<span class="asteriskField">*</span> </label> <div class=""> <input type="password" name="password" class="textinput textInput form-control" required id="id_password" /> </div> </div')
-        self.assertContains(
-            response, '<button class="btn btn-outline-info" type="submit">Login</button>')
+        self.assertContains(response, '<div id="div_id_username" class="form-group"> <label for="id_username" class="col-form-label  requiredField">')
+        self.assertContains(response, '<span class="asteriskField">*</span> </label> <div class=""> <input type="password" name="password" class="textinput textInput form-control" required id="id_password" /> </div> </div')
+        self.assertContains(response, '<button class="btn btn-outline-info" type="submit">Login</button>')
 
     def test_post_login_page(self):
-        response = self.client.post(reverse(
-            'login'), {'username': 'demo_user', 'password': 'demo@123'}, follow=True)
+        response = self.client.post(reverse('login'), {'username': 'demo_user', 'password': 'demo@123'}, follow=True)
         self.assertRedirects(response, reverse('home'))
 
     def test_login_with_invalid_credentials(self):
         """
         Test for user is not able to login with invalid credentials
         """
-        response = self.client.post(
-            reverse('login'), {'username': 'adafafaf', 'password': 'wrong@passowrd'})
+        response = self.client.post(reverse('login'), {'username': 'adafafaf', 'password': 'wrong@passowrd'})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context['form'].errors, {'__all__': ['Please enter a correct username and password. Note that both fields may be case-sensitive.']})
         response = self.client.post(reverse('login'), {'username': 'demo_user', 'password': '1sfsdf'})
