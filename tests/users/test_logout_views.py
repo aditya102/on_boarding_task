65372a2208ef61e69ec1d207a1bcb38baac0d2f0
aditya102 <aditya@testpress.com>
2019-02-12 17:01:26 +0530

    Update login test and logout test
    - Added doc string to each method
    - used assertFormError for form testing

--- tests/users/test_logout_views.py
+++ tests/users/test_logout_views.py
@@ -5,12 +5,9 @@ from django.test import TestCase
 
 class AccountsTestCase(TestCase):
     def setUp(self):
-        user = {
-            'username': 'demo_user'
-        }
-        User.objects.create_user(username=user['username'], password='demo@123')
+        self.user = User.objects.create_user(username='demo_user', password='demo@123')
 
     def test_logout(self):
-        self.client.login(username='demo_user', password='demo@123')
+        self.client.login(username=self.user.username, password='demo@123')
         response = self.client.get(reverse('logout'), follow=True)
         self.assertRedirects(response, '/users/login/')
