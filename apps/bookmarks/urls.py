b337ca3dd8dacfda7738f08e1524e8aa74e7e909
aditya102 <aditya@testpress.in>
2019-02-26 14:30:47 +0530

    Updat Folder delete and bookmark delete

--- apps/bookmarks/urls.py
+++ apps/bookmarks/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('folder/', views.FolderListView.as_view(), name="folders_list"),
     path('folder/<int:folder_id>/', views.BookmarkListView.as_view(), name='folder_detail'),
     path('create/', views.FolderCreateView.as_view(), name="folder_create"),
+    path('<int:folder_id>/delete/', views.FolderDeleteView.as_view(), name="folder_delete"),
     path('folder/<int:folder_id>/create/', views.BookmarkCreateView.as_view(), name="bookmark_create"),
-]   
+    path('folder/<int:bookmark_id>/delete', views.BookmarkDeleteView.as_view(), name='bookmark_delete'),
+    path('<folder_id>/edit/', views.FolderUpdateView.as_view(), name="folder_update"),
+]
--- apps/bookmarks/views.py
+++ apps/bookmarks/views.py
@@ -1,10 +1,13 @@
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import DeleteView
+from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.shortcuts import get_object_or_404
 
 from .models import Folder, Bookmark
-from .forms import FolderCreateForm, BookmarkCreateForm
+from .forms import FolderCreateForm, BookmarkCreateForm, FolderUpdateForm
 
 
 class FolderListView(LoginRequiredMixin, ListView):
@@ -12,7 +15,6 @@ class FolderListView(LoginRequiredMixin, ListView):
     model = Folder
     template_name = 'bookmarks/folder_list.html'
     context_object_name = 'folders'
-    paginate_by = 10
     ordering = ['name']
 
 
@@ -58,3 +60,72 @@ class BookmarkCreateView(LoginRequiredMixin, CreateView):
     def get_success_url(self):
         url = reverse('bookmarks:folders_list')
         return url
+
+
+class FolderDeleteView(LoginRequiredMixin, DeleteView):
+    model = Folder
+    template_name = 'bookmarks/folder_delete_confirm.html'
+
+    def get_object(self):
+        id = self.kwargs.get('folder_id')
+        return get_object_or_404(Folder, id=id)
+
+    def get_success_url(self):
+        return reverse('bookmarks:folders_list')
+
+    def delete(self, request, *args, **kwargs):
+        folder = self.get_object()
+
+        if folder.name == "Uncategorized":
+            for bookmark in folder.bookmark_set.all():
+                bookmark.delete()
+            folder.delete()
+            return HttpResponseRedirect(reverse('bookmarks:folders_list'))
+
+        if Folder.objects.filter(name='Uncategorized', created_by=self.request.user).exists():
+            uncategorized_folder = Folder.objects.get(name="Uncategorized", created_by=self.request.user)
+        else:
+            uncategorized_folder = Folder.objects.create(name='Uncategorized', created_by=self.request.user)
+
+        for bookmark in folder.bookmark_set.all():
+            bookmark.folder = uncategorized_folder
+            bookmark.save()
+        folder.delete()
+        return HttpResponseRedirect(reverse('bookmarks:folders_list'))
+
+
+class BookmarkDeleteView(LoginRequiredMixin, DeleteView):
+    model = Bookmark
+    template_name = 'bookmarks/bookmark_delete_confirm.html'
+
+    def get_object(self):
+        id = self.kwargs.get('bookmark_id')
+        return get_object_or_404(Bookmark, id=id)
+
+    def get_success_url(self):
+        return reverse('bookmarks:folders_list')
+
+        def get_queryset(self):
+            qs = super().get_queryset()
+            return qs.filter(created_by=self.request.user)
+
+
+class FolderUpdateView(LoginRequiredMixin, UpdateView):
+    model = Folder
+    form_class = FolderUpdateForm
+    template_name = 'bookmarks/folder_update_form.html'
+
+    def get_object(self):
+        id = self.kwargs.get('folder_id')
+        return get_object_or_404(Folder, id=id)
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return kwargs
+
+    def get_queryset(self):
+        return Folder.objects.filter(created_by=self.request.user)
+
+    def get_success_url(self):
+        return reverse('bookmarks:folders_list')
